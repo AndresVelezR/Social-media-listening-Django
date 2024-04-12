f47a5068f55eb4d9ab195c23169b864f17d01295
samargo <samargo0317@gmail.com>
2024-04-11 20:05:00 -0500

    Adding Scraper for comment extraction

---
+++ TrendPulse/Scraper/settings.py
@@ -0,0 +1,9 @@
+SUBJECT = 'westcol'
+SCROLL_PAUSE_TIME = 3
+SCROLL = 1000
+SCROLL_SCRIPT = f'window.scrollBy(0, {SCROLL});'
+PATH_CHROMEDRIVER = '/home/samargo/Documents/selenium/chromedriver'
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
+QUANTITY_TWEETS = 10
+NAME_FILE = "out.json"
