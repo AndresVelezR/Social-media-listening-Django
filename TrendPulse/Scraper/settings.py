a9cc52af11eacb1e823659a81804e2c97728255b
samargo <samargo0317@gmail.com>
2024-05-01 14:55:08 -0500

    Adding scraper developed with JavaScript cause' it's faster than the older version developed with Selenium

--- TrendPulse/Scraper/settings.py
+++
@@ -1,9 +0,0 @@
-SUBJECT = 'westcol'
-SCROLL_PAUSE_TIME = 3
-SCROLL = 1000
-SCROLL_SCRIPT = f'window.scrollBy(0, {SCROLL});'
-PATH_CHROMEDRIVER = '/home/samargo/Documents/selenium/chromedriver'
-WINDOW_WIDTH = 1920
-WINDOW_HEIGHT = 1080
-QUANTITY_TWEETS = 10
-NAME_FILE = "out.json"
